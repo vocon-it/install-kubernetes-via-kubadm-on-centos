ee63c6bcb6e4891c32de927cac2dac4d78a9afbd
Oliver Veits @ vocon <oliver.veits@vocon-it.com>
2020-04-30 13:34:58 +0000

    TODO: verify, whether the iptables commands are needed and that they do not break anything

--- 2_install_kubeadm/2_reset_kubeadm.sh
+++ 2_install_kubeadm/2_reset_kubeadm.sh
@@ -3,10 +3,12 @@
 set -euxo pipefail
 
 sudo kubeadm reset --force
-sudo iptables -F
-sudo iptables -t nat -F
-sudo iptables -t mangle -F
-sudo iptables -X
+
+# TODO: verify, whether the iptables commands are needed and that they do not break anything
+#sudo iptables -F
+#sudo iptables -t nat -F
+#sudo iptables -t mangle -F
+#sudo iptables -X
 
 # TODO: test/improve create iptables
 bash $(cd $(dirname $0); pwd)/../3_create_iptables_entries/1_create_iptables_entries.sh
