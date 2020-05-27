6202e12815a353abf5698f8620be6ae343abf00a
Rodrigues-Gabriel <52155147+Rodrigues-Gabriel@users.noreply.github.com>
2020-05-27 16:13:23 -0300

    removing unnecessary lines

--- 2_install_kubeadm/2_reset_kubeadm.sh
+++ 2_install_kubeadm/2_reset_kubeadm.sh
@@ -4,12 +4,5 @@ set -euxo pipefail
 
 sudo kubeadm reset --force
 
-# TODO: verify, whether the iptables commands are needed and that they do not break anything
-#sudo iptables -F
-#sudo iptables -t nat -F
-#sudo iptables -t mangle -F
-#sudo iptables -X
-
 # TODO: test/improve create iptables
 bash $(cd $(dirname $0); pwd)/../3_create_iptables_entries/1_create_iptables_entries.sh
-
