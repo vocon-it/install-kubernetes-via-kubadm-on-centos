69f4872982cb83f086b36850d529dc9fcba9df08
Oliver Veits <oliver.veits@gmail.com>
2020-06-01 13:46:59 +0200

    running 8_update_certificates.sh as centos user

--- 8_update_certificates.sh
+++ 8_update_certificates.sh
@@ -6,31 +6,37 @@
 # TODO: run this as non-root using sudo
 [ "$(id -u)" != "0" ] && echo "$0 must be run as root. Exiting..." && exit 1
 
+sudo echo test 2>/dev/null 1>/dev/null || alias sudo='$@'
+
 # Detect own IP address and check that it is a correct IPv4 address:
-API_IP=$(/sbin/ifconfig eth0 | grep -v inet6 | grep inet | awk '{print $2}') \
+API_IP=$(sudo /sbin/ifconfig eth0 | grep -v inet6 | grep inet | awk '{print $2}') \
   && echo "$API_IP" | egrep -v "^[1-9][0-9]{0,2}\.[1-9][0-9]{0,2}.[1-9][0-9]{0,2}.[1-9][0-9]{0,2}$" \
   && echo "ERROR: could not find IP address. Exiting ..." && exit 1
 
 # Create backup of the Certificates:
-cd /etc/kubernetes/pki/ \
-  && mkdir -p ~/kubernetes-pki.bak \
-  && mv {apiserver.crt,apiserver-etcd-client.key,apiserver-kubelet-client.crt,front-proxy-ca.crt,front-proxy-client.crt,front-proxy-client.key,front-proxy-ca.key,apiserver-kubelet-client.key,apiserver.key,apiserver-etcd-client.crt} ~/kubernetes-pki.bak/ \
+sudo mkdir -p /root/kubernetes-pki.bak
+  && sudo mv /etc/kubernetes/pki/{apiserver.crt,apiserver-etcd-client.key,apiserver-kubelet-client.crt,front-proxy-ca.crt,front-proxy-client.crt,front-proxy-client.key,front-proxy-ca.key,apiserver-kubelet-client.key,apiserver.key,apiserver-etcd-client.crt} /root/kubernetes-pki.bak/
+
+#cd /etc/kubernetes/pki/ \
+#  && mkdir -p ~/kubernetes-pki.bak \
+#  && mv {apiserver.crt,apiserver-etcd-client.key,apiserver-kubelet-client.crt,front-proxy-ca.crt,front-proxy-client.crt,front-proxy-client.key,front-proxy-ca.key,apiserver-kubelet-client.key,apiserver.key,apiserver-etcd-client.crt} ~/kubernetes-pki.bak/ \
 
 # Create new Certificates:
-kubeadm init phase certs all --apiserver-advertise-address $API_IP
+sudo kubeadm init phase certs all --apiserver-advertise-address $API_IP
 
 # Create Backup of the old configurations:
-cd /etc/kubernetes/ \
-  && mv {admin.conf,controller-manager.conf,kubelet.conf,scheduler.conf} ~/kubernetes-pki.bak/
+sudo mv /etc/kubernetes/{admin.conf,controller-manager.conf,kubelet.conf,scheduler.conf} /root/kubernetes-pki.bak/
 
 # Create new configuration files:
-kubeadm init phase kubeconfig all \
+sudo kubeadm init phase kubeconfig all \
   && mkdir -p $HOME/.kube \
   && sudo cp /etc/kubernetes/admin.conf $HOME/.kube/config \
   && sudo chown $(id -u):$(id -g) $HOME/.kube/config
-#  && sudo mkdir -p /root/.kube \
-#  && sudo cp /etc/kubernetes/admin.conf /root/.kube/config
+  && sudo mkdir -p /root/.kube \
+  && sudo cp /etc/kubernetes/admin.conf /root/.kube/config
 
 
 # reboot needed:
-reboot
+a=no
+read -p "A reboot is needed. Rebooting now? (yes|no) > " a
+[ "$(echo $a | cut -c 1 | awk '{print tolower($0)}')" == "y" ] && sudo reboot
