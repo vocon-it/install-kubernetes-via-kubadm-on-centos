8052d97ddd91ddb7bc26134c9f1c97b96cbb7e4f
Oliver Veits @ vocon <oliver.veits@vocon-it.com>
2020-10-20 15:02:39 +0000

    troubelshooting echos

--- 1_install_docker_18.06.sh
+++ 1_install_docker_18.06.sh
@@ -11,7 +11,7 @@ yum check-update
 # install docker v 18.06, which is compatible with latest kubectl 
 
 echo "--- Add the Docker repository ---"
-sudo tee /etc/yum.repos.d/docker.repo <<-'EOF' 
+sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
 [docker-ce-edge]
 name=Docker CE Edge - $basearch
 baseurl=https://download.docker.com/linux/centos/7/$basearch/edge
@@ -20,21 +20,33 @@ gpgcheck=1
 gpgkey=https://download.docker.com/linux/centos/gpg
 EOF
 
+echo "\$?=$?"
+
 echo "--- Install Docker CE ---"
-sudo yum install -y docker-ce-18.06.1.ce-3.el7.x86_64 
+sudo yum install -y docker-ce-18.06.1.ce-3.el7.x86_64
+
+echo "\$?=$?"
 
 echo "--- Allow sudo rights of docker service ---"
 sudo usermod -aG docker $(whoami)
+
+echo "\$?=$?"
 
 echo "--- Start docker now ---"
 sudo systemctl start docker
 sudo systemctl status docker
 
+echo "\$?=$?"
+
 echo "--- Start docker automatically after boot ---"
 sudo systemctl enable docker
+
+echo "\$?=$?"
 
 echo 'Docker should be installed now. Try with "sudo docker search hello".'
 echo 'After logout and login again, "sudo" will not be needed anymore'
 
 echo "--- Print installed Docker Version ---"
 sudo docker --version
+
+echo "\$?=$?"
