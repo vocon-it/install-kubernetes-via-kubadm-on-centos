741a4fbdcbabfb96b67c8ff19b95a112ab59c41e
Gabriel Rodrigues <gabriel.rodrigues@vocon-it.com>
2020-12-21 21:22:50 +0100

    better way of seeing how many pods are running

--- 7_install_cert_manager/1_install_cert-manager.sh
+++ 7_install_cert_manager/1_install_cert-manager.sh
@@ -1,4 +1,4 @@
-kubectl get pods --namespace cert-manager | awk '{print $2}' | grep 1/1 && INSTALLED=true
+kubectl get pods --namespace cert-manager | awk '{print $2}' | grep -c 1/1 | grep 3 && INSTALLED=true
 
 if [ "$INSTALLED" != "true" ]; then
   kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml
