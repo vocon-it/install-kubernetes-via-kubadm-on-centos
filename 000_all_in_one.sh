144c280cc0900c311fb4b2e1722b8468857b37de
Oliver Veits @ vocon <oliver.veits@vocon-it.com>
2020-12-28 16:09:52 +0000

    MASTER and AGENT detection

--- 000_all_in_one.sh
+++ 000_all_in_one.sh
@@ -3,6 +3,14 @@
 # Stop on Error
 set -e
 
+# Roles
+hostname | grep master && MASTER=true || true
+hostname | grep node && AGENT=true || true
+[ "${MASTER}" == "" ] && [ "${AGENT}" == "" ]  && MASTER=true && AGENT=true || true
+
+echo "MASTER=${MASTER}"
+echo "AGENT=${AGENT}"
+
 # Define sudo, if it does not yet exist:
 sudo echo nothing 2>/dev/null 1>/dev/null || alias sudo='$@'
 
@@ -32,7 +40,11 @@ echo "--------------------"
 echo "--- INIT KUBEADM ---"
 echo "--------------------"
 echo
-bash 2_install_kubeadm/3_initialize_kubeadm.sh || false
+if [ "${MASTER}" == "true" ]; then
+  bash 2_install_kubeadm/3_initialize_kubeadm.sh || false
+else
+  echo "The node is no master. Skipping this step."
+fi
 
 echo "------------------------------"
 echo "--- DEPLOY OVERLAY NETWORK ---"
@@ -44,7 +56,12 @@ echo "----------------------"
 echo "--- UNTAINT MASTER ---"
 echo "----------------------"
 echo
-bash 2_install_kubeadm/5_untaint_master.sh || false
+if [ "${MASTER}" == "true" ] && [ "${AGENT}" == "true" ]; then
+  bash 2_install_kubeadm/5_untaint_master.sh || false
+else
+  echo "The node is no master and agent. Skipping this step."
+fi
+
 
 # CREATE PERSISTENT VOLUMES 
 # ----- SWITCHED OFF -----
