49cf81c7fc1af7670e540d2f5b640c2109e34d68
Gabriel Rodrigues <gabriel.rodrigues@vocon-it.com>
2021-01-21 16:29:00 +0100

    cert manager installation verifications

--- 7_install_cert_manager/1_install_cert-manager.sh
+++ 7_install_cert_manager/1_install_cert-manager.sh
@@ -1,10 +1,29 @@
-kubectl get pods --namespace cert-manager | awk '{print $2}' | grep -c 1/1 | grep 3 && INSTALLED=true
+checkCertManagerInstallation() {
+  kubectl get pods --namespace cert-manager | awk '{print $2}' | grep -c 1/1 | grep 3
+}
+
+checkCertManagerInstallation && INSTALLED=true
 
 if [ "$INSTALLED" != "true" ]; then
   kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yaml
+else
+  exit 0
 fi
+
+DEPLOYMENT_INFO=""
 
 for (( i=0; i < 10; i++ ))
 do
-  echo $i
+  DEPLOYMENT_INFO=$(checkCertManagerInstallation)
+  echo "${DEPLOYMENT_INFO}"
+  if [ "${DEPLOYMENT_INFO}" -eq 3 ]; then
+    break
+  fi
+  sleep 5 
 done
+
+if [ ! "${DEPLOYMENT_INFO}" -eq 3 ]; then
+  echo "CertManager Installation was not successful"
+  exit 1
+fi
+
