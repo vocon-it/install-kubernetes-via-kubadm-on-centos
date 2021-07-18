032180900f29fafd6eca74cd8dece90c32160e86
oliver.veits@dev-node2.vocon-it.com <oliver.veits@dev-node2.vocon-it.com>
2021-07-18 13:39:32 +0200

    CAAS-797 corrected 99_uninstall_kubeadm_and_docker.sh script

--- 99_uninstall_kubeadm_and_docker.sh
+++ 99_uninstall_kubeadm_and_docker.sh
@@ -1,4 +1,14 @@
+#!/usr/bin/env bash -ex
 
-sudo yum remove -y kubelet kubeadm kubctl
-DOCKER_OLD=$(sudo yum list installed | grep docker | awk '{print $1}')
-[ "${DOCKER_OLD}" != "" ] && sudo yum remove -y "${DOCKER_OLD}"
+DATETIME=$(date +"%Y-%m-%d--%H-%M-%S")
+mv ~/.kube/config ~/.kube/config-${DATETIME} || true
+sudo mv /root/.kube/config /root/.kube/config-${DATETIME} || true
+
+sudo yum list installed | egrep "docker|kubelet|kubeadm|kubectl" | awk '{print $1}'
+
+OLD_PACKAGES=$(sudo yum list installed | egrep "docker|kubelet|kubeadm|kubectl" | awk '{print $1}')
+for PACKAGE in ${OLD_PACKAGES}; do
+  echo "PACKAGE=${PACKAGE}"
+  sudo yum remove -y "${PACKAGE}" && echo removed package ${PACKAGE}
+done
+
