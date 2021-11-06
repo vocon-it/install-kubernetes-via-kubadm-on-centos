db6fb87943cb50dfc398f993bdd0597b986601ef
Oliver Veits @ vocon <oliver.veits@vocon-it.com>
2021-11-06 01:52:51 +0000

    CAAS-945 added release-2.0 support to all in one script

--- 5_install_ingress/000_nginxinc_all_in_one.sh
+++ 5_install_ingress/000_nginxinc_all_in_one.sh
@@ -3,9 +3,11 @@
 # Official Documentation: see https://docs.nginx.com/nginx-ingress-controller/installation/installation-with-manifests
 
 [ "$1" == "-d" ] && CMD=delete || CMD=apply
-#RELEASE=master
-RELEASE=release-1.9 # Note: '1.9' is a moving target and it refers to 1.9.1, currently (as of 2021-01-03)
-RAW_URL="https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${RELEASE}/deployments"
+#NGINXINC_BRANCH=master
+#NGINXINC_BRANCH=${NGINXINC_BRANCH:=release-1.9} # Note: '1.9' is a moving target and it refers to 1.9.1, currently (as of 2021-01-03)
+NGINXINC_BRANCH=${NGINXINC_BRANCH:=release-2.0} # Note: '2.0' is a moving target and it refers to 2.0.3, currently (as of 2021-11-06)
+BASE_URL="https://raw.githubusercontent.com/nginxinc/kubernetes-ingress/${NGINXINC_BRANCH}/deployments"
+
 
 # choose the environment, where to deploy the NginX controller by choosing the KUBECONFIG file:
 DEPLOY_ON_ENVIRONMENT=${DEPLOY_ON_ENVIRONMENT:=local}
@@ -14,37 +16,60 @@ toLower() {
 }
 export KUBECONFIG=${KUBECONFIG:=$( [ "${DEPLOY_ON_ENVIRONMENT}" != "local" ] && echo ~/.kube/${DEPLOY_ON_ENVIRONMENT}-config | toLower || echo ~/.kube/config )}
 
-NAMESPACE_AND_SERVICEACCOUNT=common/ns-and-sa.yaml
-RBAC="rbac/rbac.yaml rbac/ap-rbac.yaml"
-COMMOM="common/default-server-secret.yaml common/nginx-config.yaml common/ingress-class.yaml"
-CUSTOM_RESOURCES="common/vs-definition.yaml common/vsr-definition.yaml common/ts-definition.yaml common/policy-definition.yaml common/gc-definition.yaml common/global-configuration.yaml"
-APP_PROJECT="common/ap-logconf-definition.yaml common/ap-policy-definition.yaml"
-NGINX_INGRESS=daemon-set/nginx-ingress.yaml
-ALL="$NAMESPACE_AND_SERVICEACCOUNT $RBAC $COMMOM $CUSTOM_RESOURCES $APP_PROJECT $NGINX_INGRESS"
+if [ "${NGINXINC_BRANCH}" == "release-1.9" ]; then
+  NAMESPACE_AND_SERVICEACCOUNT=common/ns-and-sa.yaml
+  RBAC="rbac/rbac.yaml rbac/ap-rbac.yaml"
+  COMMOM="common/default-server-secret.yaml common/nginx-config.yaml common/ingress-class.yaml"
+  CUSTOM_RESOURCES="common/vs-definition.yaml common/vsr-definition.yaml common/ts-definition.yaml common/policy-definition.yaml common/gc-definition.yaml common/global-configuration.yaml"
+  APP_PROJECT="common/ap-logconf-definition.yaml common/ap-policy-definition.yaml"
+  NGINX_INGRESS=daemon-set/nginx-ingress.yaml
+  ALL="$NAMESPACE_AND_SERVICEACCOUNT $RBAC $COMMOM $CUSTOM_RESOURCES $APP_PROJECT $NGINX_INGRESS"
+else # see https://docs.nginx.com/nginx-ingress-controller/installation/installation-with-manifests as of 2021-11-06
+  ALL="
+      common/ns-and-sa.yaml
+      rbac/rbac.yaml
+      rbac/ap-rbac.yaml
+      common/default-server-secret.yaml
+      common/nginx-config.yaml
+      common/ingress-class.yaml
+      common/crds/k8s.nginx.org_virtualservers.yaml
+      common/crds/k8s.nginx.org_virtualserverroutes.yaml
+      common/crds/k8s.nginx.org_transportservers.yaml
+      common/crds/k8s.nginx.org_policies.yaml
+      common/crds/k8s.nginx.org_globalconfigurations.yaml
+      common/crds/appprotect.f5.com_aplogconfs.yaml
+      common/crds/appprotect.f5.com_appolicies.yaml
+      common/crds/appprotect.f5.com_apusersigs.yaml
+      daemon-set/nginx-ingress.yaml
+    "
+fi
 
 for YAML in $ALL
 do
   if [ "${YAML}" == "daemon-set/nginx-ingress.yaml" ]; then
-    curl -s "${RAW_URL}/${YAML}" | sed 's/terminationMessagePolicy: File/terminationMessagePolicy: FallbackToLogsOnError/' \
-     | kubectl ${CMD} -f -
+    curl -s "${BASE_URL}/${YAML}" \
+      | sed 's/terminationMessagePolicy: File/terminationMessagePolicy: FallbackToLogsOnError/' \
+      | sed 's/^        args:/        args:\n          - -enable-snippets/' \
+      | kubectl ${CMD} -f -
   else
-    kubectl ${CMD} -f "${RAW_URL}/${YAML}"
+    kubectl ${CMD} -f "${BASE_URL}/${YAML}"
   fi
 done
 
-verify_success() {
-  kubectl get pod -n nginx-ingress | grep Running
-}
+if [ "${CMD}" == "apply" ]; then
+  verify_success() {
+    kubectl get pod -n nginx-ingress | grep Running
+  }
 
-for I in $(seq 1 10); do
-  verify_success && exit 0
-  sleep 10
-done
+  for I in $(seq 1 10); do
+    verify_success && exit 0
+    sleep 10
+  done
 
-echo "POD does not seem to start. Showing log."
-POD=$(kubectl get pod -n nginx-ingress | grep nginx-ingress | head -1 | awk '{print $1}')
-kubectl -n nginx-ingress describe pod $POD | grep -A 100 "Events:"
+  echo "POD does not seem to start. Showing log."
+  POD=$(kubectl get pod -n nginx-ingress | grep nginx-ingress | head -1 | awk '{print $1}')
+  kubectl -n nginx-ingress describe pod $POD | grep -A 100 "Events:"
 
-echo "If you reach here, then something went wrong in $0"
-exit 1
-
+  echo "If you reach here, then something went wrong in $0"
+  exit 1
+fi
