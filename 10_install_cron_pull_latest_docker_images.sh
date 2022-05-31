8991f70a35ce26e6093f95c140c4acae4123af1e
Oiver Veits <oliver.veits@vocon-it.com>
2022-05-31 18:12:38 +0200

    CAAS-1237 pull latest docker image migrated from docker to containerd ctr

--- 10_install_cron_pull_latest_docker_images.sh
+++ 10_install_cron_pull_latest_docker_images.sh
@@ -5,7 +5,7 @@ set -e
 
 usage() {
   echo "usage: [DEBUG=true] $0 schedule install_file_path [image1 [image2 ...]]"
-  echo "e.g.:  bash $0 '*/15 * * * *' /root/cron_pull_latest.sh vocon/intellij-desktop vocon/deploy-intellij-desktop"
+  echo "e.g.:  bash $0 '*/15 * * * *' /root/cron_pull_latest.sh vocon/intellij-desktop:latest vocon/deploy-intellij-desktop:latest vocon/idle-timeout:latest"
 }
 
 if [ $# -lt 2 ]; then
@@ -21,16 +21,17 @@ IMAGES="$@"
 cat <<'EOF' > ${SCRIPT_FILE}
 usage() {
   echo "usage: $0 [image1 [image2] ...]"
-  echo "e.g.:  $0 vocon/intellij-desktop vocon/deploy-intellij-desktop"
+  echo "e.g.:  $0 vocon/intellij-desktop:latest vocon/deploy-intellij-desktop:latest vocon/idle-timeout:latest"
 }
 
 # split input parameters in lines:
 LATEST_IMAGES="$(echo $@ | tr ' ' '\n')"
 
 # add latest images found via 'docker images':
-docker login
+#docker login
 LATEST_IMAGES="${LATEST_IMAGES}
 $(docker images | grep latest | awk '{print $1}')
+$(sudo ctr -n k8s.io images ls | grep develop | awk '{print $1}' | sed 's_docker.io/__')
 "
 
 # remove duplicates:
@@ -39,7 +40,9 @@ LATEST_IMAGES="$(echo "${LATEST_IMAGES}" | egrep -v '^[ ]*$' | sort | uniq)"
 # pull:
 for LATEST_IMAGE in $LATEST_IMAGES
 do
+  echo $LATEST_IMAGE | grep -q ':' || LATEST_IMAGE=$LATEST_IMAGE:latest 
   docker pull $LATEST_IMAGE
+  sudo ctr -n k8s.io images pull docker.io/$LATEST_IMAGE
 done
 EOF
 
