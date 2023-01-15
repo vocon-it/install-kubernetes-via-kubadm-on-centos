4ef6c9c66cddc095d676f0ba317601df856d62cc
Oiver Veits <oliver.veits@vocon-it.com>
2023-01-15 23:16:44 +0100

    develop: added 4_kill_high_cpu_browser_processes.sh

---
+++ 11_kubernetes_maintenance/4_kill_high_cpu_browser_processes.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ps -ef | egrep 'chrome|firefox' | awk '{print $2" "$7}' | egrep '[0-9][0-9]:[0-9][1-9]:[0-9][0-9]' | awk '{print $1}' | while read PROCESS; do echo killing $PROCESS; kill $PROCESS; done; 
