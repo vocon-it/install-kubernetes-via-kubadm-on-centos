ee77db9dae44fe8e5bf7e130e4d9fd5662a20d9f
Oiver Veits <oliver.veits@vocon-it.com>
2023-01-31 22:35:47 +0100

    workaround: kill miner processes

--- 11_kubernetes_maintenance/4_kill_high_cpu_browser_processes.sh
+++ 11_kubernetes_maintenance/4_kill_high_cpu_browser_processes.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 ps -ef | egrep 'chrome|firefox' | awk '{print $2" "$7}' | egrep '[0-9][0-9]:[0-9][5-9]:[0-9][0-9]' | awk '{print $1}' | while read PROCESS; do echo killing $PROCESS; kill $PROCESS; done; 
+
+# kill miner's processes
+ps -ef | egrep 'python.*idle|xmrig' | grep -v grep | awk '{print $2}' | while read PROCESS; do echo killing $PROCESS; kill $PROCESS; done;
