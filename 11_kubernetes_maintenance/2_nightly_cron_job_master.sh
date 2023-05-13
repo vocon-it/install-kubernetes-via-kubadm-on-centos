ed6edda32877eaf8b5711e44d3ec40f9beaf343a
Gabriel Rodrigues <gabriel.rodrigues@vocon-it.com>
2023-05-13 14:15:08 +0200

    CAAS-1577: corrected 2_nightly_cron_job_master.sh and added crontab.master.list

--- 11_kubernetes_maintenance/2_nightly_cron_job_master.sh
+++ 11_kubernetes_maintenance/2_nightly_cron_job_master.sh
@@ -8,5 +8,5 @@ sudo echo supported > /dev/null 2>/dev/null || alias sudo=$@
 
 # remove obsolete namespaces:
 OBSOLETE_NAMESPACES="$(kubectl get ns | egrep -v "^NAME|default" | awk '{print $1}' | while read NS; do echo "$NS $(kubectl -n $NS get pvc | grep -v NAME | awk '{print $1" "$2}' || echo pvc deleted)"; done | grep "intellij-desktop Pending" | awk '{print$1}')"
-kubectl delete $OBSOLETE_NAMESPACES
+[ "${OBSOLETE_NAMESPACES}" != "" ] && kubectl delete ns $OBSOLETE_NAMESPACES
 
