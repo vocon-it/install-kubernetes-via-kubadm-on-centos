c939c5cf046087848aec875aec1250a3084c27e9
Gabriel Rodrigues <gabriel.rodrigues@vocon-it.com>
2023-09-18 11:54:29 +0200

    minor edit

--- 11_kubernetes_maintenance/2_nightly_cron_job_master.sh
+++ 11_kubernetes_maintenance/2_nightly_cron_job_master.sh
@@ -3,22 +3,26 @@
 #
 # run in the directory, where $0 is located:
 #
+
 DIR="$(cd $(dirname $0); pwd)" && cd "${DIR}" 
 
 #
 # check, if sudo is supported:
 #
+
 sudo echo supported > /dev/null 2>/dev/null || alias sudo=$@
 
 #
 # remove obsolete namespaces:
 #
+
 OBSOLETE_NAMESPACES="$(kubectl get ns | egrep -v "^NAME|default" | awk '{print $1}' | while read NS; do echo "$NS $(kubectl -n $NS get pvc | grep -v NAME | awk '{print $1" "$2}' || echo pvc deleted)"; done | grep "intellij-desktop Pending" | awk '{print$1}')"
 [ "${OBSOLETE_NAMESPACES}" != "" ] && kubectl delete ns $OBSOLETE_NAMESPACES
 
 #
 # remove obsolete ingresses:
 #
+
 NAMESPACES_TO_BE_EXCLUDED_BECAUSE_IS_ACTIVE="$(kubectl top pod --all-namespaces --use-protocol-buffers --sort-by=memory | egrep ' intellij-desktop' | awk '{print $1}')"
 
 if [ "$NAMESPACES_TO_BE_EXCLUDED_BECAUSE_IS_ACTIVE" != "" ]; then
