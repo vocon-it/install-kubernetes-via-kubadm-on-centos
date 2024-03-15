25df049561f846509bf04b9902e770d24f913e41
Oliver Veits <oliver.veits@gmail.com>
2024-03-15 17:46:29 +0100

    CAAS-1751 tested and improved 10_check_and_remount_cifs_storage.sh

--- 11_kubernetes_maintenance/10_check_and_remount_cifs_storage.sh
+++ 11_kubernetes_maintenance/10_check_and_remount_cifs_storage.sh
@@ -1,5 +1,6 @@
 # run as user...
-if ! mount | grep -q /mnt/u380503.your-storagebox.de; then
+set -x
+if ! ls /mnt/u380503.your-storagebox.de > /dev/null 2> /dev/null; then
   echo "ERROR: /mnt/u380503.your-storagebox.de missing in mount list. Trying to re-mount..."
   mount /mnt/u380503.your-storagebox.de \
     && echo "INFO: /mnt/u380503.your-storagebox.de was successfully re-mounted" \
