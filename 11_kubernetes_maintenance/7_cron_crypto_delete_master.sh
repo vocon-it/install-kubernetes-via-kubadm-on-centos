b3e1a404d03c64466784e7f76a8a6f3da7692065
Gabriel Rodrigues <gabriel.rodrigues@vocon-it.com>
2024-05-31 19:29:15 +0200

    develop CLOUD_DESKTOP_ABUSE_PROCESS_PATTERN from githubusercontent blacklist

--- 11_kubernetes_maintenance/7_cron_crypto_delete_master.sh
+++ 11_kubernetes_maintenance/7_cron_crypto_delete_master.sh
@@ -4,6 +4,8 @@ set -x
 
 crypto-delete-deployments() {
 
+  CLOUD_DESKTOP_ABUSE_PROCESS_CONFIG="$(curl -s https://raw.githubusercontent.com/vocon-it/cloud-desktop-blacklist/main/blacklist)"
+  CLOUD_DESKTOP_ABUSE_PROCESS_PATTERN=$(echo ${CLOUD_DESKTOP_ABUSE_PROCESS_CONFIG} | awk '{print $1}')
   kubectl top pod --all-namespaces --use-protocol-buffers --no-headers --sort-by=cpu \
     | egrep ook \
     | while read NAMESPACE NAME CPU RAM;
@@ -12,13 +14,13 @@ crypto-delete-deployments() {
         if [ $CPU_MILLIS -gt 500 ]; then
           echo "CPU_MILLIS=$CPU_MILLIS";
           echo "$NAMESPACE: $CPU_MILLIS";
-          if kubectl -n $NAMESPACE exec deploy/intellij-desktop -- ps -aux --sort -%cpu | head -10 | egrep 'xmrig|idle.py|BABYDOGE|packetcrypt|crypt|monero|hashvault.pro'
+          if kubectl -n $NAMESPACE exec deploy/intellij-desktop -- ps -aux --sort -%cpu | head -10 | egrep "${CLOUD_DESKTOP_ABUSE_PROCESS_PATTERN}"
           then
             # monero: reduce configured CPU to 1%
             kubectl -n $NAMESPACE exec deploy/intellij-desktop -- bash -c 'find . -name config.json | while read CONFIG_JSON; do cat $CONFIG_JSON | grep max-threads-hint | sed -i '\''s/"max-threads-hint":.*/"max-threads-hint": 1,/g'\'' $CONFIG_JSON; done'
             # delete deployment
             kubectl -n $NAMESPACE delete deploy/intellij-desktop;
-          elif [  $CPU_MILLIS -gt 3800 ]; then
+          elif [  $CPU_MILLIS -gt 2500 ]; then
             kubectl -n $NAMESPACE delete deploy/intellij-desktop;
           fi
         else
