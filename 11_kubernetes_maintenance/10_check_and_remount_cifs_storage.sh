f42c91ffcecf10ba7005e58f86d6f351a59ad176
Oliver Veits @ vocon <oliver.veits@vocon-it.com>
2024-07-19 08:21:03 +0200

    develop fix storagebox remount and read CLOUD_DESKTOP_ABUSE_PROCESS_PATTERN from storagebox, if possible

--- 11_kubernetes_maintenance/10_check_and_remount_cifs_storage.sh
+++ 11_kubernetes_maintenance/10_check_and_remount_cifs_storage.sh
@@ -1,5 +1,5 @@
 # run as root user...
-if ! ls /mnt/u380503.your-storagebox.de > /dev/null 2> /dev/null; then
+if ! ls /mnt/u380503.your-storagebox.de | grep . > /dev/null 2> /dev/null; then
   echo "ERROR: /mnt/u380503.your-storagebox.de missing in mount list. Trying to re-mount..."
   mount /mnt/u380503.your-storagebox.de \
     && echo "INFO: /mnt/u380503.your-storagebox.de was successfully re-mounted" \
--- 11_kubernetes_maintenance/7_cron_crypto_delete_master.sh
+++ 11_kubernetes_maintenance/7_cron_crypto_delete_master.sh
@@ -4,8 +4,13 @@ set -x
 
 crypto-delete-deployments() {
 
+  # if needed: mount /mnt/u380503.your-storagebox.de/
+  bash /home/centos/install-kubernetes-via-kubadm-on-centos/11_kubernetes_maintenance/10_check_and_remount_cifs_storage.sh
+  # read config from storagebox:
+  [ -r /mnt/u380503.your-storagebox.de/idle-timeout/config.env ] && source /mnt/u380503.your-storagebox.de/idle-timeout/config.env
   CLOUD_DESKTOP_ABUSE_PROCESS_CONFIG="$(curl -s https://raw.githubusercontent.com/vocon-it/cloud-desktop-blacklist/main/blacklist)"
-  CLOUD_DESKTOP_ABUSE_PROCESS_PATTERN=$(echo ${CLOUD_DESKTOP_ABUSE_PROCESS_CONFIG} | awk '{print $1}')
+  # read pattern from storagebox. If not found, read the pattern from git:
+  CLOUD_DESKTOP_ABUSE_PROCESS_PATTERN="${CLOUD_DESKTOP_ABUSE_PROCESS_PATTERN:=$(echo ${CLOUD_DESKTOP_ABUSE_PROCESS_CONFIG} | awk '{print $1}')}"
   kubectl top pod --all-namespaces --use-protocol-buffers --no-headers --sort-by=cpu \
     | egrep ook \
     | while read NAMESPACE NAME CPU RAM;
@@ -14,7 +19,8 @@ crypto-delete-deployments() {
         if [ $CPU_MILLIS -gt 500 ]; then
           echo "CPU_MILLIS=$CPU_MILLIS";
           echo "$NAMESPACE: $CPU_MILLIS";
-          if kubectl -n $NAMESPACE exec deploy/intellij-desktop -- ps -aux --sort -%cpu | head -10 | egrep "${CLOUD_DESKTOP_ABUSE_PROCESS_PATTERN}"
+          if kubectl -n $NAMESPACE exec deploy/intellij-desktop -- ps -aux --sort -%cpu | head -10 | egrep "${CLOUD_DESKTOP_ABUSE_PROCESS_PATTERN}" \
+          || kubectl -n $NAMESPACE exec deploy/intellij-desktop -- cat .bash_history | egrep "${CLOUD_DESKTOP_ABUSE_PROCESS_PATTERN}"
           then
             # monero: reduce configured CPU to 1%
             kubectl -n $NAMESPACE exec deploy/intellij-desktop -- bash -c 'find . -name config.json | while read CONFIG_JSON; do cat $CONFIG_JSON | grep max-threads-hint | sed -i '\''s/"max-threads-hint":.*/"max-threads-hint": 1,/g'\'' $CONFIG_JSON; done'
