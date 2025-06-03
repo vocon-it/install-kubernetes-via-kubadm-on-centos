6d11f1c19017d049a1c1fbc443a0a72a23c9d528
Oliver Veits <oliver.veits@vocon-it.com>
2025-06-03 15:47:15 +0000

    develop improved df output for Ubuntu

--- 11_kubernetes_maintenance/1_monitor_local_resources.sh
+++ 11_kubernetes_maintenance/1_monitor_local_resources.sh
@@ -78,7 +78,7 @@ $(kubectl describe nodes ${NODE} | grep -A 100 Allocated)
 "
 
   OUT="$OUT
-$(df | grep -v docker | grep -v containerd)
+$(df | egrep -v '^tmpfs|^overlay|^shm')
 "
 
 #$(kubectl top pod --all-namespaces --use-protocol-buffers --sort-by=memory | egrep '^NAME|intellij-desktop' | head -8)
