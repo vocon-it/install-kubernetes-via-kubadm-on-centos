8530d51caee0a3bba087960c0776d068c21bc38e
Oliver Veits <oliver.veits@vocon-it.com>
2026-03-23 08:54:19 +0100

    develop more verbose output for 11_kubernetes_maintenance/12_clean_obsolete_jobs_and_deployments.sh

--- 11_kubernetes_maintenance/12_clean_obsolete_jobs_and_deployments.sh
+++ 11_kubernetes_maintenance/12_clean_obsolete_jobs_and_deployments.sh
@@ -16,15 +16,18 @@ echo "--------------------------------"
      print_number_of_pods_on_all_hosts
 echo "--------------------------------"
 
-echo "remove old jobs that were not completed (i.e. errored and pending jobs)"
+echo -n "INFO: removing old jobs that were not completed (i.e. errored and pending jobs)..."
 kubectl -n deploy-intellij-desktop get job | grep 0/1 | egrep '[0-9]{2,2}m$' | cut -d ' ' -f 1 | while read JOB; do kubectl -n deploy-intellij-desktop delete job/$JOB; done
+echo "done"
 
-echo "scale down old Pending intellij-desktops:"
+echo-n "INFO: scaling down old Pending intellij-desktops..."
 kubectl get deploy -A | grep 0/1 | grep intellij-desktop | egrep '[0-9]{2,2}m$' | while read NAMESPACE DEPLOY REST; do kubectl -n $NAMESPACE scale deploy/$DEPLOY --replicas=0; done
+echo "done"
 
-echo "scale down obsolete idle-timeout deployments:"
+echo -n "INFO: scaling down obsolete idle-timeout deployments..."
 EXCLUDE_PATTERN=$(kubectl get deploy -A | grep /1 | grep intellij-desktop | cut -d' ' -f 1 | tr '\n' '|' | sed 's/|$//')
 kubectl -n idle-timeout get deploy | grep /1 | egrep -v "${EXCLUDE_PATTERN}" | while read DEPLOY REST; do kubectl -n idle-timeout scale deploy/$DEPLOY --replicas=0; done
+echo "done"
 
 echo "--------------------------------"
 echo "After cleaning: Number of PODS on all hosts:"
