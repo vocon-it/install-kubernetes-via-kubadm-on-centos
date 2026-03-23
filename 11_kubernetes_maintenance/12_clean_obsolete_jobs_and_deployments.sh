6b6f8054e794c34ca7d636e52ac5d7b383267495
Oliver Veits <oliver.veits@vocon-it.com>
2026-03-23 08:47:17 +0100

    develop adapt 11_kubernetes_maintenance/12_clean_obsolete_jobs_and_deployments.sh to run on Singapore cluster

--- 11_kubernetes_maintenance/12_clean_obsolete_jobs_and_deployments.sh
+++ 11_kubernetes_maintenance/12_clean_obsolete_jobs_and_deployments.sh
@@ -1,12 +1,22 @@
 # master: ~/install-kubernetes-via-kubadm-on-centos/11_kubernetes_maintenance/12_clean_obsolete_jobs_and_deployments.sh
-echo Number of PODS:
-for NODE in node1 node2
-do
-NUMBER_OF_PODS=$(kubectl get pod -A -o wide | egrep 'Running|Pending' | grep $NODE | wc -l)
-echo "${NODE}: ${NUMBER_OF_PODS}"
-done
 
-# remove old jobs thet were not completed (i.e. errored and pending jobs)
+print_number_of_pods_on_all_hosts() {
+    # Print number of pods on all hosts:
+    NODES="$(kubectl get nodes -o json | jq -r .items[].metadata.name)"
+    for NODE in ${NODES}
+    do
+        NUMBER_OF_PODS=$(kubectl get pod -A -o wide | egrep 'Running|Pending' | grep $NODE | wc -l)
+        echo "${NODE}: ${NUMBER_OF_PODS}"
+    done
+}
+
+echo "--------------------------------"
+echo "Before cleaning: Number of PODS on all hosts:"
+echo "--------------------------------"
+     print_number_of_pods_on_all_hosts
+echo "--------------------------------"
+
+# remove old jobs that were not completed (i.e. errored and pending jobs)
 kubectl -n deploy-intellij-desktop get job | grep 0/1 | egrep '[0-9]{2,2}m$' | cut -d ' ' -f 1 | while read JOB; do kubectl -n deploy-intellij-desktop delete job/$JOB; done
 
 # scale down old Pending intellij-desktops:
@@ -16,12 +26,10 @@ kubectl get deploy -A | grep 0/1 | grep intellij-desktop | egrep '[0-9]{2,2}m$' 
 EXCLUDE_PATTERN=$(kubectl get deploy -A | grep /1 | grep intellij-desktop | cut -d' ' -f 1 | tr '\n' '|' | sed 's/|$//')
 kubectl -n idle-timeout get deploy | grep /1 | egrep -v "${EXCLUDE_PATTERN}" | while read DEPLOY REST; do kubectl -n idle-timeout scale deploy/$DEPLOY --replicas=0; done
 
-# Print number of pods on this host:
-echo Number of remaining PODS:
-for NODE in node1 node2
-do
-NUMBER_OF_PODS=$(kubectl get pod -A -o wide | egrep 'Running|Pending' | grep $NODE | wc -l)
-echo "${NODE}: ${NUMBER_OF_PODS}"
-done
+echo "--------------------------------"
+echo "After cleaning: Number of PODS on all hosts:"
+echo "--------------------------------"
+     print_number_of_pods_on_all_hosts
+echo "--------------------------------"
 
 
