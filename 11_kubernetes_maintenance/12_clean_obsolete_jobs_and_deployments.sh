26cd4f8892f175176ea3bf0f07e060cbdee12057
Oliver Veits <oliver.veits@vocon-it.com>
2026-03-23 08:52:17 +0100

    develop more verbose output for 11_kubernetes_maintenance/12_clean_obsolete_jobs_and_deployments.sh

--- 11_kubernetes_maintenance/12_clean_obsolete_jobs_and_deployments.sh
+++ 11_kubernetes_maintenance/12_clean_obsolete_jobs_and_deployments.sh
@@ -16,13 +16,13 @@ echo "--------------------------------"
      print_number_of_pods_on_all_hosts
 echo "--------------------------------"
 
-# remove old jobs that were not completed (i.e. errored and pending jobs)
+echo "remove old jobs that were not completed (i.e. errored and pending jobs)"
 kubectl -n deploy-intellij-desktop get job | grep 0/1 | egrep '[0-9]{2,2}m$' | cut -d ' ' -f 1 | while read JOB; do kubectl -n deploy-intellij-desktop delete job/$JOB; done
 
-# scale down old Pending intellij-desktops:
+echo "scale down old Pending intellij-desktops:"
 kubectl get deploy -A | grep 0/1 | grep intellij-desktop | egrep '[0-9]{2,2}m$' | while read NAMESPACE DEPLOY REST; do kubectl -n $NAMESPACE scale deploy/$DEPLOY --replicas=0; done
 
-# scale down obsolete idle-timeout deployments:
+echo "scale down obsolete idle-timeout deployments:"
 EXCLUDE_PATTERN=$(kubectl get deploy -A | grep /1 | grep intellij-desktop | cut -d' ' -f 1 | tr '\n' '|' | sed 's/|$//')
 kubectl -n idle-timeout get deploy | grep /1 | egrep -v "${EXCLUDE_PATTERN}" | while read DEPLOY REST; do kubectl -n idle-timeout scale deploy/$DEPLOY --replicas=0; done
 
